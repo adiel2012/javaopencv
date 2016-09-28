f048cd97f8f2e54a4b3087ce9edf57030f1065d3
acastano <acastano@GYECDOC6PC16.ups.edu.ec>
2016-09-28 15:16:35 -0500

    trabajando

--- src/samples/LWF.java
+++ src/samples/LWF.java
@@ -7,11 +7,21 @@ package samples;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 /**
  *
@@ -22,9 +32,10 @@ public class LWF {
     public static void main(String[] args) {
         System.out.println("Hello, OpenCV");
         // Load the native library.
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        System.load("D:\\opencv_instalaciones\\opencv\\build\\java\\x64\\opencv_java310.dll");
 
-        String urlHelen = "C:\\Users\\LABORATORIO 5\\Downloads\\helen\\trainset";
+        String urlHelen = "D:\\borrar\\helen\\trainset";
         File carpeta = new File(urlHelen);
         File[] images = carpeta.listFiles(new FilenameFilter() {
             @Override
@@ -34,18 +45,20 @@ public class LWF {
             }
         });
 
+        File carpetaalmacen = new File("D:\\generated");
+        if (carpetaalmacen.exists() == false) {
+            carpetaalmacen.mkdir();
+        }
+        
+        int[][] delaunay_triangles = new int[200][3];
         for (File image : images) {
             System.out.println(image.getName());
             String puntos_file = image.getName().substring(0, image.getName().length() - 3) + "pts";
             System.out.println(puntos_file);
             System.out.println("------------------------");
-            double[][][] puntos = leerpuntos(new File(urlHelen+"\\"+ puntos_file));
-//            Rect[] rectangles = detect_faces(image);
-//            for (Rect rectangle : rectangles) {
-//
-//            }
-
-
+            double[][] puntos = leerpuntos(new File(urlHelen + "\\" + puntos_file));
+            Mat mat = Imgcodecs.imread(image.getAbsolutePath());
+            save_meshed_images(puntos, carpetaalmacen, image, mat, delaunay_triangles);
 
         }
 
@@ -67,7 +80,34 @@ public class LWF {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private static double[][][] leerpuntos(File file) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private static double[][] leerpuntos(File file) {
+        double[][] res = new double[68][2];
+        try {
+            Scanner scan = new Scanner(file);
+            String trash = scan.nextLine();
+            trash = scan.nextLine();
+            trash = scan.nextLine();
+
+            for (int i = 0; i < 68; i++) {
+                String[] numbers = scan.nextLine().split(" ");
+                res[i][0] = Double.parseDouble(numbers[0]);
+                res[i][1] = Double.parseDouble(numbers[1]);
+            }
+
+            scan.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(LWF.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return res;
+    }
+
+    private static void save_meshed_images(double[][] puntos, File carpetaalmacen, File image, Mat mat, int[][] delaunay_triangles) {
+        Mat mat_copy = mat.clone();
+        int radii = 1000;
+        for (double[] punto : puntos) {
+            Imgproc.ellipse(mat_copy, new Point(punto),new Size(radii, radii),0,0,0,new Scalar(0, 255, 0));
+        }
+        Imgcodecs.imwrite(carpetaalmacen.getAbsolutePath() + "\\" + image.getName(), mat_copy);
     }
 }
